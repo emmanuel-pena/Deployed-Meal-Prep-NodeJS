69865c6c7fb86628dfd70b5b3ed93e11be9bb851
emmanuel-pena <99593740+emmanuel-pena@users.noreply.github.com>
2022-07-04 22:57:09 -0700

    Delete backend/frontend directory

--- backend/frontend/src/components/globalContext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const globalContext = React.createContext({});
-export const GlobalProvider = globalContext.Provider;
-export default globalContext;
-
-
--- backend/frontend/src/components/resetpassword.style.js
+++
@@ -1,9 +0,0 @@
-export const inputLabel = {
-  fontSize: {
-    xs: 15,
-    sm: 18,
-     md: 21,
-      lg: 24,
-    },
-    pt: 3,
-};
